70c22ca5fb77a5394a955759f8ce632c3ecdd90d
nuvo17~ <eluyemiselim16@gmail.com>
2023-11-17 11:25:12 +0100

    added to java api

--- index.js
+++ index.js
@@ -1,23 +1,64 @@
 const btn = document.querySelector('button')
-const inputText = document.querySelector('#hi')
+const inputText = document.querySelector('#text-input')
 const display = document.querySelector('h2')
+const dollar = document.querySelector("#USD")
+const south = document.querySelector("#ZAR")
 
 
 
 const selim = async()=> {
-    const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    const response = await fetch("https://api.frankfurter.app/latest?from=ZAR")
     const result = await response.json()
-    
-    return(result.bpi.USD.rate_float)
-  
+    const value = result;
+
+    return value.rates.USD;
+
   } 
 
-  btn.addEventListener("click", sync ()=> {
-    let newResult = await selim()
-    const NigerianButton = document.querySelector('#NGN')
-    const AmericanButton = document.querySelector('#USD')
+  const exchange = async()=> {
+    const response = await fetch("https://api.frankfurter.app/latest?from=ZAR")
+    const total = await response.json()
+    const zarValue = result;
 
 
+    return zarValue.rates.ZAR;
+      
+  } 
+
+  const selimExchange = async()=> {
+    const response = await fetch("https://api.frankfurter.app/latest?from=ZAR")
+    const result = await response.json()
+    const value = result;
+
+    return value.base;
+
+  } 
+
+  const zarExchange = async()=> {
+    const response = await fetch("https://api.frankfurter.app/latest?from=ZAR")
+    const total = await response.json()
+    const zarValue = result;
+
+
+    return zarValue.base;
+  }
+
+
+  
+  south.addEventListener("click", sync ()=> {
+    let zValue = await zarExchange()
+    let zBase = await exchange();
+    btn.addEventListener("click", async () => {
+      display.textContent = input.value * zBase + zValue
+      input.value = ""
+    })
   })
   
-
+  dollar.addEventListener("click", sync ()=> {
+    let dValue = await zarExchange()
+    let dBase = await exchange();
+    btn.addEventListener("click", async () => {
+      display.textContent = input.value * dBase + dValue
+      input.value = ""
+    })
+  })
